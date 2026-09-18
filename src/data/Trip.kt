44573a498868fdda686f36742f3d2bab26745ed4
NebulousPrime26 <t.vaniperen2001@gmail.com>
2026-09-18 23:03:01 +0100

    renamed "calculatedMileage" to "distance"

--- src/data/Trip.kt
+++ src/data/Trip.kt
@@ -14,6 +14,6 @@ data class Trip(
     val privateUse: Boolean = false,
     val notes: String = "",
 ) {
-    val distanceMileage: Double
+    val distance: Double
         get() = endMileage - startMileage
 }
