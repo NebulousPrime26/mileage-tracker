b837d085ad5c39c4714b0547fb370d77e770e573
NebulousPrime26 <t.vaniperen2001@gmail.com>
2026-09-18 23:01:02 +0100

    added trip object

---
+++ src/data/Trip.kt
@@ -0,0 +1,16 @@
+package com.nebulousprime26.mileage_tracker.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "trips")
+data class Trip(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val date: Long,
+    val startPostalCode: String = "",
+    val endPostalCode: String = "",
+    val startMileage: Double,
+    val endMileage: Double,
+    val privateUse: Boolean = false,
+    val notes: String = "",
+)
